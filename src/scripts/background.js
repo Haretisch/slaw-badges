db95eaadaf617766dda695d229c292a9243dc2f9
Haretisch <contact@firehawkes.com>
2018-01-16 22:03:00 -0500

    Completed firefox support, I think...

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,4 +1,5 @@
 const system = getSystem();
+const targetUrl = 'https://www.twitch.tv/sirslaw';
 
 // Shows icon as active only when on https://www.twitch.tv/sirslaw
 if(system.declarativeContent){
@@ -7,11 +8,21 @@ if(system.declarativeContent){
       system.declarativeContent.onPageChanged.addRules([{
         conditions: [
           new system.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'https://www.twitch.tv/sirslaw' },
+            pageUrl: { urlContains: targetUrl },
           })
         ],
         actions: [ new system.declarativeContent.ShowPageAction() ]
       }]);
     });
+  });
+} else {
+  function initializePageAction(tab) {
+    if(tab.url.includes(targetUrl)){
+        system.pageAction.show(tab.id);
+    }
+  }
+
+  system.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+    initializePageAction(tab);
   });
 }
